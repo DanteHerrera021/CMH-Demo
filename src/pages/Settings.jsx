660955acf85611ea636ab1fc9323a889aa2df6ab
Dante Herrera <dherrera3@my.wctc.edu>
2026-03-13 15:02:28 -0500

    add settings page formatting

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,15 +4,19 @@ import Button from "../components/ui/Button";
 export default function Settings() {
   return (
     <PageContainer>
-      <div className="mt-6 d-flex flex-col md:flex-row gap-4">
+      <div className="mt-6 flex flex-col md:flex-row md:justify-between md:align-center gap-4">
         <div>
           <h1 className="text-3xl font-bold mb-1">Settings</h1>
           <p className="text-slate-500 mb-6">
             Configure your media hub preferences.
           </p>
         </div>
-        <div>
-          <Button text="Save Settings" />
+        <div className="">
+          <Button
+            text="Save Settings"
+            rounded="sm"
+            className="bg-brand-primary text-white"
+          />
         </div>
       </div>
 
